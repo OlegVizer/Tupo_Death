9bd1c62d75311b71a9d77fc01255efcecd3c2139
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:51:01 +0300

    1.1

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -6,15 +6,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class KoscheiTheDeathless {
-
-    private Ocean1 ocean;
-
-    public String getRulesByDeth() {
-        return "На свете есть океан , " + ocean.toString();
-    }
-
     @Autowired
-    public void setOcean(Ocean1 ocean) {
-        this.ocean = ocean;
+    private  Ocean1 ocean1;
+    public String getRulesByDeth() {
+        return "На свете есть океан , " + ocean1.toString();
     }
 }
+
--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -5,16 +5,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Wood3 {
-
-    private final Rabbit4 rabbit;
-
     @Autowired
-    public Wood3(Rabbit4 rabbit) {
-        this.rabbit = rabbit;
-    }
-
+    private Rabbit4 rabbit4;
     @Override
     public String toString() {
-        return ", на дереве заяц " + rabbit.toString();
+        return ", на дереве заяц " + rabbit4.toString();
     }
 }
